324521c618a1c25774461aa56504339588b8f634
majo <64664464+majo1569@users.noreply.github.com>
2022-09-26 16:35:15 -0300

    Update mic-led.ino

--- mic-led.ino
+++ mic-led.ino
@@ -1,13 +1,17 @@
+#include <arduino-timer.h>
+
+auto timer = timer_create_default(); // create a timer with default settings
+int pico = 0;
 int led1 = D2;
 int led2 = D3;
 int led3 = D4;
 int led4 = D5;
 int led5 = D6;
-byte rele = D7;
+int rele = D7;
 int sonido_digital = D1;
 int sonido_analogico = A0;
 
-void setup(){
+void setup() {
   Serial.begin(115200);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
@@ -15,77 +19,87 @@ void setup(){
   pinMode(led4, OUTPUT);
   pinMode(led5, OUTPUT);
   pinMode(rele, OUTPUT);
-  pinMode(sonido_digital, INPUT);  
+  pinMode(sonido_digital, INPUT); 
+
+  timer.every(3000, toggle_led);
+
 }
 
-void loop(){
+bool toggle_led(void *) {
+  
+  if (pico > 2){
+    digitalWrite (led1, HIGH);
+    digitalWrite (led2, HIGH);
+    digitalWrite (led3, HIGH);
+    digitalWrite (led4, HIGH);
+    digitalWrite (led5, HIGH);
+    Serial.println("ACTIVAR EL RELE AHORA");
+    digitalWrite (rele, HIGH);
+    delay(2000);
+    digitalWrite (rele, LOW);
+    delay(2000);
+    Serial.print("\t");
+    Serial.println("reinicie pico");
+    digitalWrite (led1, LOW);
+    digitalWrite (led2, LOW);
+    digitalWrite (led3, LOW);
+    digitalWrite (led4, LOW);
+    digitalWrite (led5, LOW);
+  }
+  pico=0;
+  return true; // keep timer active? true
+}
+
+
+
+void loop() {
   int val_digital = digitalRead(sonido_digital);
   int val_analogico = analogRead(sonido_analogico);
 
   Serial.print(val_analogico);
   Serial.print("\t");
   Serial.println(val_digital);
+  Serial.print("\t");
+  Serial.println(pico);
 
-//CONTROL DE LEDS SEGUN VOLUMEN DETECTADO
-  if (val_analogico < 500)
-  {
+  timer.tick(); // tick the timer
+  if (val_analogico<700){
     digitalWrite (led1, HIGH);
     digitalWrite (led2, HIGH);
     digitalWrite (led3, HIGH);
     digitalWrite (led4, HIGH);
     digitalWrite (led5, HIGH);
+    pico ++;
   }
-  else if (val_analogico < 600)
-   {
+  if (val_analogico < 735){
     digitalWrite (led1, HIGH);
     digitalWrite (led2, HIGH);
     digitalWrite (led3, HIGH);
     digitalWrite (led4, HIGH);
     digitalWrite (led5, LOW);
-   }
-   else if (val_analogico < 700)
-    {
+
+  }
+  else if (val_analogico < 755){
     digitalWrite (led1, HIGH);
     digitalWrite (led2, HIGH);
     digitalWrite (led3, HIGH);
     digitalWrite (led4, LOW);
     digitalWrite (led5, LOW);
-    }
-   else if (val_analogico < 800)
-    {
+  }
+    else if (val_analogico < 775){
     digitalWrite (led1, HIGH);
     digitalWrite (led2, HIGH);
     digitalWrite (led3, LOW);
     digitalWrite (led4, LOW);
     digitalWrite (led5, LOW);
-    }
-   else if (val_analogico < 900)
-    {
+  }
+    else if (val_analogico < 800){
     digitalWrite (led1, HIGH);
     digitalWrite (led2, LOW);
     digitalWrite (led3, LOW);
     digitalWrite (led4, LOW);
     digitalWrite (led5, LOW);
-    }
-  else if (val_analogico < 1025)
-    {
-    digitalWrite (led1, LOW);
-    digitalWrite (led2, LOW);
-    digitalWrite (led3, LOW);
-    digitalWrite (led4, LOW);
-    digitalWrite (led5, LOW);
-    }
+  
+  }
+  
 }
-
-
-// CONTROL DE RELE SEGUN THRESHOLD
-
-//  if (val_digital == HIGH)
-//  {
-//    digitalWrite (rele, HIGH);
-//    delay(5000);
-//    }
-//  else
-//  {
-//   digitalWrite (rele, LOW);
-//    }
